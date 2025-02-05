f137d2efe2bd28d3f95202f05bc96f35d9771eb6
ratnanil <rata@zhaw.ch>
2025-02-05 13:01:51 +0100

    refactor

--- server.R
+++ server.R
@@ -2,11 +2,11 @@
 source("libraries.R")
 source("utils.R")
 
-datasets <- c("normallandschaft")
+# datasets <- c("normallandschaft")
 
-names(datasets) <- datasets
+# names(datasets) <- datasets
 
-dataset_list <- read_csv("appdata/infoflora.csv")
+# dataset_list <- read_csv("appdata/infoflora.csv")
 
 mycols <- list(
   drawing = list(
@@ -38,38 +38,24 @@ shinyServer(function(input, output) {
         group = "Pixelkarte farbig"
       ) |>
       addLayersControl(baseGroups = c("Pixelkarte grau", "Pixelkarte farbig", "Swissimage")) |>
-      fitBounds(5.955902, 45.81796, 10.49206, 47.80845) #|>
-      # set singleFeature = TRUE to only disaable multi-feature drawing
-      # addDrawToolbar(
-      #   polylineOptions = FALSE,
-      #   polygonOptions = FALSE,
-      #   circleOptions = FALSE,
-      #   markerOptions = FALSE,
-      #   circleMarkerOptions = FALSE,
-      #   singleFeature = TRUE,  # turn this back on if we want to be able to draw polygons
-      #   rectangleOptions = drawRectangleOptions(
-      #     shapeOptions = drawShapeOptions(
-      #       color = as.character(mycols$drawing$hex),
-      #       fill = FALSE,
-      #       weight = 2
-      #     )
-      #   ),
-      #   editOptions = editToolbarOptions()
-      # )
+      fitBounds(5.955902, 45.81796, 10.49206, 47.80845)
+
   })
   geodata_i <- reactive({
-    # select_dataset(geodata, input$aggregation, input$datensatz)
     geodata[[input$aggregation]]
   })
 
-  dataset_i <- reactive({
-    dataset_list
-  })
+
 
   observe({
     
     # browser()
     geodata_i <- geodata_i()
+    
+    leafletProxy("map") |> 
+      clearShapes() |> 
+      clearControls() |> 
+      clearMarkers()
     
     
     
@@ -77,15 +63,14 @@ shinyServer(function(input, output) {
       
       if(input$column_y == "n"){
         
-        leafletProxy("map", data = geodata_i) |>
-          clearShapes() |>
-          clearControls() |>
+        leafletProxy("map", data = geodata_i) |> 
           addCircleMarkers(
-            fillColor = "black",
-            radius = 2,
-            color = "black", 
-            fillOpacity = 1, 
-            opacity = 1) 
+            fillColor = "darkgreen",
+            radius = 3,
+            weight = 1,
+            color = "black",
+            fillOpacity = .6, 
+            opacity = .8) 
         
       } else{
         
@@ -101,8 +86,6 @@ shinyServer(function(input, output) {
         column_y <- names(col_y_options[col_y_options == input$column_y])
         
         leafletProxy("map", data = geodata_i) |>
-          clearShapes() |>
-          clearControls() |>
           addCircleMarkers(
             fillColor = ~pal(ycol),
             radius = 2,
@@ -121,8 +104,8 @@ shinyServer(function(input, output) {
       if(input$column_y == "n"){
         
         
-        
-        pal <- colorNumeric("viridis", range(geodata_i$n))
+        # browser()
+        pal <- colorNumeric("viridis", range(geodata_i$n, na.rm = TRUE),na.color = "#80808000")
         
         
         # browser()
@@ -131,8 +114,6 @@ shinyServer(function(input, output) {
         )
         
         leafletProxy("map", data = geodata_i) |>
-          clearShapes() |>
-          clearControls() |>
           addPolygons(
             fillColor = ~ pal(n),
             color = ~ pal(n),
@@ -148,6 +129,7 @@ shinyServer(function(input, output) {
         
       } else{
         
+        # browser()
         ycol <- geodata_i[[input$column_y]]
         
         n_obs <- geodata_i[["n"]]
@@ -179,23 +161,18 @@ shinyServer(function(input, output) {
         geodata_i$grp <- factor(paste(n_obs_grp, ycol_grp, sep = "-"), levels = fac_levels)
         
         
-        # mypal <- rev(RColorBrewer::brewer.pal(n_classes, "RdYlBu"))
         mypal <- c("#91BFDB", "#FFFFBF", "#FC8D59")
         
         bivariate_matrix <- bivariate_matrix_alpha(mypal, n_classes, alpha_range = c(.40, 0.95))
         
-        
         column_y <- names(col_y_options[col_y_options == input$column_y])
-        
-        
+      
         legend_html <- create_legend(bivariate_matrix,column_y)
         
         pal_col <- as.vector(bivariate_matrix)
-        pal <- colorFactor(pal_col, levels = fac_levels, alpha = TRUE)
+        pal <- colorFactor(pal_col, levels = fac_levels, alpha = TRUE,na.color = "#80808000")
         
         leafletProxy("map", data = geodata_i) |>
-          clearShapes() |>
-          clearControls() |>
           addControl(legend_html, position = "bottomleft", className = "") |>
           addPolygons(
             fillColor = ~ pal(grp),
@@ -222,9 +199,7 @@ shinyServer(function(input, output) {
     } else{
     
       selvec <- as.vector(geodata_i[, input$aggregation, drop = TRUE]) == selected_object()
-      
-      
-      # browser()
+    
       leafletProxy("map", data = geodata_i[selvec, ]) |>
         clearGroup("polygonselection") |>
         addPolygons(
@@ -232,7 +207,6 @@ shinyServer(function(input, output) {
           group = "polygonselection", 
           color = mycols$selected_polygon$hex, 
           fill = FALSE,
-          # label = n
           )  
     }
     
@@ -255,164 +229,25 @@ shinyServer(function(input, output) {
     })
   })
 
-  grassland_inbounds <- reactive({
-    if (length(ranges()) > 0) {
-      ranges <- ranges()[[1]]
-      lat <- ranges[, 2]
-      lng <- ranges[, 1]
-      dataset_i() |>
-        filter(
-          lange > min(lng),
-          lange < max(lng),
-          breite > min(lat),
-          breite < max(lat)
-        )
-    } else {
-      dataset_i()[FALSE, ]
-    }
-  })
 
 
 
 
-  # observeEvent(input$map_shape_click,{browser()})
 
-  # Makes sure that this object exists even before the first clicking event
+
   selected_object <- reactiveVal("")
-  # Reactivate the next lines if clicking on a shape should highlight points in the plot 
-  # observeEvent(input$map_shape_click, {
-  #   loc_list <- input$map_shape_click
-  #   loc_list <- input$map_shape_click
-  #   geodata_i <-
-  #     select_dataset(geodata, input$aggregation, input$datensatz)
-  #   loc <- st_point(c(loc_list$lng, loc_list$lat)) |>
-  #     st_sfc(crs = 4326)
-  # 
-  #   selected_object_str <-
-  #     as.vector(geodata_i[loc, input$aggregation, drop = TRUE])
-  #   selected_object(selected_object_str) # sets the value of this reactiveValue
-  # })
 
 
-  grassland_renamed <- reactive({
-    dataset_i() |>
-      rename(column_y = input$column_y) #|>
-      # rename(agg = input$aggregation)
-  })
+
+
 
   grassland_inbounds_renamed <- reactive({
     grassland_inbounds <- grassland_inbounds() |>
       rename(column_y = input$column_y)
-    # grassland_inbounds <-
-      # grassland_inbounds |> rename(agg = input$aggregation)
+
 
     return(grassland_inbounds)
   })
 
 
-  # Removed plot for now, since it does not make sense in the current state:
-  # https://github.com/zhaw-biodiversity-monitor/zhaw-biodiversity-monitor.github.io/issues/10
-  # output$scatterplot <- renderPlotly({
-  #   fig <-
-  #     plot_ly(
-  #       grassland_renamed(),
-  #       x = ~jahr,
-  #       y = ~column_y,
-  #       type = "scatter",
-  #       mode = "markers",
-  #       marker = list(color = "rgba(255, 182, 193, 1)"),
-  #       name = "all"
-  #     ) |>
-  #     add_trace(
-  #       data = grassland_inbounds_renamed(),
-  #       color = "",
-  #       marker = list(
-  #         color = "rgba(255,255,255,0)",
-  #         line = list(color = mycols$drawing$rgba_string, width = 2)
-  #       ),
-  #       name = "in bounds"
-  #     )
-  #   if (selected_object() != "") {
-  #     grassland_inpolygon <- grassland_renamed()[grassland_renamed()$agg == selected_object(), ]
-  # 
-  #     fig <-
-  #       fig |>
-  #       add_trace(
-  #         data = grassland_inpolygon,
-  #         color = "",
-  #         marker = list(
-  #           color = "rgba(255,255,255,0)",
-  #           line = list(color = mycols$selected_polygon$rgba_string, width = 2)
-  #         ),
-  #         name = "in polygon"
-  #       )
-  #   }
-  # 
-  #   fig |>
-  #     layout(
-  #       hovermode = FALSE,
-  #       clickmode = "none",
-  #       # yaxis = list(title = paste0(clean_names(input$column_y), add_unit(input$column_y))),
-  #       yaxis = list(title = paste0(clean_names(input$column_y))),
-  #       xaxis = list(title = "Erhebungszeitpunkt"),
-  #       modebar = list(
-  #         remove = c(
-  #           "autoScale2d",
-  #           "autoscale",
-  #           "editInChartStudio",
-  #           "editinchartstudio",
-  #           "hoverCompareCartesian",
-  #           "hovercompare",
-  #           "lasso",
-  #           "lasso2d",
-  #           "orbitRotation",
-  #           "orbitrotation",
-  #           "pan",
-  #           "pan2d",
-  #           "pan3d",
-  #           "reset",
-  #           "resetCameraDefault3d",
-  #           "resetCameraLastSave3d",
-  #           "resetGeo",
-  #           "resetSankeyGroup",
-  #           "resetScale2d",
-  #           "resetViewMapbox",
-  #           "resetViews",
-  #           "resetcameradefault",
-  #           "resetcameralastsave",
-  #           "resetsankeygroup",
-  #           "resetscale",
-  #           "resetview",
-  #           "resetviews",
-  #           "select",
-  #           "select2d",
-  #           "sendDataToCloud",
-  #           "senddatatocloud",
-  #           "tableRotation",
-  #           "tablerotation",
-  #           "toImage",
-  #           "toggleHover",
-  #           "toggleSpikelines",
-  #           "togglehover",
-  #           "togglespikelines",
-  #           "toimage",
-  #           "zoom",
-  #           "zoom2d",
-  #           "zoom3d",
-  #           "zoomIn2d",
-  #           "zoomInGeo",
-  #           "zoomInMapbox",
-  #           "zoomOut2d",
-  #           "zoomOutGeo",
-  #           "zoomOutMapbox",
-  #           "zoomin",
-  #           "zoomout",
-  #           "displaylogo"
-  #         )
-  #       )
-  #     )
-  # })
-  # 
-  
-  
 })
